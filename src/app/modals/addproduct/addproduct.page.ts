6a403a078e11058597e33ece314b0e98a3024716
charles emieux <emieux.charles@gmail.com>
2021-06-21 17:18:51 +0200

    overflow inventory managment

--- src/app/modals/addproduct/addproduct.page.ts
+++ src/app/modals/addproduct/addproduct.page.ts
@@ -25,7 +25,7 @@ export class AddproductPage implements OnInit {
     this.modalTitle = this.navParams.data.paramTitle;
   }
 
-  order() {
+  addProduct() {
     this.afDB.list('Products').push({
       Name: this.productName,
       Desc: this.productDesc,
